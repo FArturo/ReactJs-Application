c6c92b8ff9476176d1b3ba65e2329520145ead65
Fernando Arias <FernandoArias92@gmail.com>
2016-11-04 01:57:04 -0400

    Added React Files for my Hello World Application

---
+++ app.js
@@ -0,0 +1,15 @@
+/* jshint esversion:6*/
+import React from 'react';
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1> Hello World </h1> <b>Bold</b>
+      </div>
+    );
+  }
+}
+
+// const App = () => <h1>Hello World Application</h1>
+
+export default App;
